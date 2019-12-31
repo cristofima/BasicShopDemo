e7cea49fb7268abc12cb5a6f21148d2a00e62d7f
Cristopher Coronado Moreira <cristofima@hotmail.com>
2019-12-30 19:19:03 -0500

    Update ProviderCategoryConfiguration

--- src/BasicShopDemo.Api/Models/EntityConfigurations/ProviderCategoryConfiguration.cs
+++ src/BasicShopDemo.Api/Models/EntityConfigurations/ProviderCategoryConfiguration.cs
@@ -7,6 +7,14 @@ namespace BasicShopDemo.Api.Models.EntityConfigurations
     {
         public void Configure(EntityTypeBuilder<ProviderCategory> builder)
         {
+            builder.HasOne(e => e.Category)
+                    .WithMany(e => e.ProvidersCategory)
+                    .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(e => e.Provider)
+                   .WithMany(e => e.ProviderCategories)
+                   .OnDelete(DeleteBehavior.Restrict);
+
             builder.HasIndex(e => new { e.ProviderId, e.CategoryId })
                 .HasName("UI_ProviderCategory")
                 .IsUnique();
