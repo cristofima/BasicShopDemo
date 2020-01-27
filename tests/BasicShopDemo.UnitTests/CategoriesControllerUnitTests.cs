41bcab75d7ec17d016816b21ff4b3fd4693f069e
Cristopher Coronado Moreira <cristofima@hotmail.com>
2020-01-26 21:58:25 -0500

    Unit Tests for CategoriesController - GetCategory Method

---
+++ tests/BasicShopDemo.UnitTests/CategoriesControllerUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using BasicShopDemo.Api.Controllers;
+using BasicShopDemo.Api.Core.Interfaces.DAO;
+using BasicShopDemo.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BasicShopDemo.UnitTests
+{
+    public class CategoriesControllerUnitTests
+    {
+        private readonly Mock<ICategoryDAO> mockCategoryDAO;
+        private readonly CategoriesController categoriesController;
+
+
+        public CategoriesControllerUnitTests()
+        {
+            mockCategoryDAO = new Mock<ICategoryDAO>();
+            categoriesController = new CategoriesController(mockCategoryDAO.Object);
+        }
+
+        [Fact]
+        public void GetCategoryShouldBeOfTypeOkObjectResult()
+        {
+            // Arrange
+            mockCategoryDAO.Setup(svc => svc.GetAll(null))
+            .Returns(new List<Category>());
+
+            // Act
+            var result = categoriesController.GetCategory(null);
+
+            // Assert
+            var viewResult = Assert.IsType<OkObjectResult>(result);
+
+            Assert.IsAssignableFrom<IEnumerable<Category>>(viewResult.Value);
+        }
+    }
+}
